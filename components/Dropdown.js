a09402a859291177e8f80eb6c5950c1344bf8cd7
Tatiana Moreira <tatianamoreira@Tatianas-MacBook-Pro.local>
2021-11-13 00:42:02 -0600

    feat: add dropdown to mobile

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -1,15 +1,18 @@
 import React from "react";
 
-function Dropdown({ data = [] }) {
+export const Dropdown = () => {
   return (
-    <div className="bg-blue-500">
-      <ul>
-        {data.map((item, i) => {
-          <li key={i}>{item}</li>;
-        })}
-      </ul>
-    </div>
+    <>
+      <form className="flex flex-col">
+        <select
+          className="border-2 text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal text-white bg-colorPop2"
+          name="selectList"
+          id="selectList"
+        >
+            <option value="option 1">Option 1</option> {" "}
+          <option value="option 2">Option 2</option>
+        </select>
+      </form>
+    </>
   );
-}
-
-export default Dropdown;
+};
--- components/PageTitle.js
+++ components/PageTitle.js
@@ -2,24 +2,8 @@ import { motion } from "framer-motion";
 
 function PageTitle({ text }) {
   return (
-    <motion.div
-      initial='hidden'
-      animate='visible'
-      variants={{
-        hidden: {
-          scale: 0.5,
-          opacity: 0,
-        },
-        visible: {
-          scale: 1,
-          opacity: 5,
-          transition: {
-            delay: 0.6,
-          },
-        },
-      }}
-    >
-      <h1 className=' font-bold text-xl text-secondary  inline '>
+    <motion.div initial={{ y: -250 }} animate={{ y: -10 }}>
+      <h1 className=" font-bold text-xl text-secondary  inline ">
         <span>{text}</span>
       </h1>
     </motion.div>
