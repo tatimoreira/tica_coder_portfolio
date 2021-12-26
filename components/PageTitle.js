5b508cf5b5424b36fa65eadf1d6cd78d9664ebbb
Tatiana Moreira <tatianamoreira@Tatianas-MacBook-Pro.local>
2021-12-25 19:41:59 -0600

    fix: animation deleted form title

--- components/PageTitle.js
+++ components/PageTitle.js
@@ -2,11 +2,11 @@ import { motion } from "framer-motion";
 
 function PageTitle({ text }) {
   return (
-    <motion.div initial={{ y: -250 }} animate={{ y: -10 }}>
+    <div>
       <h1 className=" font-bold text-xl text-secondary  inline ">
         <span>{text}</span>
       </h1>
-    </motion.div>
+    </div>
   );
 }
 
